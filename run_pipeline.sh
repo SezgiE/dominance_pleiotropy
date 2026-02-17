454c42a2e87992fd64c7ebfdd1d695d341c0c555
Sezgi <sezgiiercan@gmail.com>
2026-02-17 17:23:53 +0100

    Finalized setup for Snellius - Error fixed

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Force this specific script to use the right Python and Conda
-
-
 # Exit immediately if any command exits with a non-zero status
 set -e
 
@@ -13,24 +10,6 @@ echo ""
 # ---------------------------------------------------------
 # STEP 0: Repository & Environment Setup
 # ---------------------------------------------------------
-
-# The Ultimate Cross-Platform Conda Initializer:
-if [ -f "/sw/arch/RHEL9/EB_production/2025/software/Miniconda3/25.5.1-1/etc/profile.d/conda.sh" ]; then
-    # 1. Snellius supercomputer.
-    unset PYTHONPATH
-    unset PYTHONHOME
-    source /sw/arch/RHEL9/EB_production/2025/software/Miniconda3/25.5.1-1/etc/profile.d/conda.sh
-else
-    # 2. A normal Mac/Linux computer. Run normally.
-    eval "$(conda shell.bash hook)"
-fi
-
-
-# 1. Clone the repository if it doesn't exist
-if [ ! -d "d-ldsc" ]; then
-    echo "0. d-ldsc repository not found. Cloning..."
-    git clone https://github.com/astheeggeggs/d-ldsc.git
-fi
 
 # 2. Setup the MAIN environment (Python 3.8 for your data processing)
 if ! conda info --envs | grep -q 'main-py3'; then
@@ -44,7 +23,7 @@ if ! conda info --envs | grep -q 'main-py3'; then
     fi
     conda activate main-py3
     pip install "pandas" "numpy" "openpyxl" "xlrd==1.2.0"
-    conda deactivate
+    conda deactivate main-py3
 fi
 
 # 3. Setup the environment for LD_score calculation (Python 3.8 with downgraded libaries for get_ldscores.py)
@@ -59,7 +38,7 @@ if ! conda info --envs | grep -q 'ld_score-py3'; then
     fi
     conda activate ld_score-py3
     pip install "pandas<1.0.0" "numpy<1.20" "bitarray==2.6.0" "scipy" "nose" "pybedtools" "openpyxl" "xlrd==1.2.0"
-    conda deactivate
+    conda deactivate ld_score-py3
 fi
 
 # 4. Setup the Python 2.7 environment get_h2.py
@@ -74,7 +53,7 @@ if ! conda info --envs | grep -q 'd-ldsc-legacy'; then
        # Lock the environment (Fixed to avoid conda run!)
         conda activate d-ldsc-legacy
         conda config --env --set subdir osx-64
-        conda deactivate
+        conda deactivate d-ldsc-legacy
     else
         # Standard native Linux/Cluster installation
         conda create -n d-ldsc-legacy -c conda-forge -c bioconda -c defaults python=2.7 bitarray scipy numpy pandas pybedtools nose openpyxl xlrd=1.2.0 -y
